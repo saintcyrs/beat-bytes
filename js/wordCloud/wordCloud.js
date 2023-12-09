9852b9ce5817dd0a252f6d0c4335873f90125d64
Soleil Saint-Cyr <saintcyrs@dhcp-10-250-151-123.harvard.edu>
2023-12-09 17:15:38 -0500

    word cloud updated

--- js/wordCloud/wordCloud.js
+++ js/wordCloud/wordCloud.js
@@ -12,13 +12,14 @@ class wordCloud {
     // Initialize minimum and maximum to be updated later
     this.minMax = [0, 100];
     this.displayData = data;
+    // TODO: Possibly find another color scheme that looks better with existing theme
+    this.genreColorMap = d3.scaleOrdinal(["#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e","#e6ab02","#a6761d","#666666"]);
     this.initVis();
   }
 
   initVis() {
     let vis = this;
-
-    vis.fill = d3.scaleOrdinal(d3.schemeTableau10);
+    // TODO: On hover, change all other colors to white (is this CSS?)
 
     // define margins
     vis.margin = { top: 0, right: 20, bottom: 20, left: 20 };
@@ -40,6 +41,8 @@ class wordCloud {
       );
     
     vis.cloud = d3.layout.cloud().size([vis.width, vis.height]);
+
+    // TODO: Add tooltip on hover (# of streams)
     vis.wrangleData();
   }
 
@@ -52,17 +55,24 @@ class wordCloud {
     // Count the number of occurrences for each artist
     const counts = {};
     vis.displayData.forEach((d) => {
-      const artist = d.artist_names;
+      // TODO: Change artist to only be defined by the first artist in artist_names
+      const artist = vis.getPrimaryArtist(d);
       counts[artist] = (counts[artist] || 0) + 1;
     });
 
     // Possibly redefine this to include the artist name ??
     // BUT there are many artists who only have one song on the charts
     // Store the processed data for use in getWords
-    vis.processedData = Object.entries(counts).map(([artist, count]) => ({
-      artist_names: artist,
-      count: count,
-    }));
+    // Modify the processedData mapping
+    vis.processedData = Object.entries(counts).map(([artist, count]) => {
+      let primaryGenre = vis.getPrimaryGenre(artist).split(",")[0];
+      return {
+        artist_names: artist,
+        genre: primaryGenre,
+        count: count,
+        originalColor: vis.genreColorMap(primaryGenre) // Set the original color here
+      };
+    });
     
     console.log("PROCESSED DATA", vis.processedData);
     // Calculate the min and max counts
@@ -102,7 +112,9 @@ class wordCloud {
     let vis = this;
     return words.map(d => ({
       text: d.artist_names,
-      size: vis.wordScale(d.count)
+      genre: d.genre,
+      size: vis.wordScale(d.count),
+      color: d.originalColor,
     }));
   }
 
@@ -112,15 +124,48 @@ class wordCloud {
     let text = vis.svg.selectAll("text")
       .data(words, d => d.text);
 
+    // At the beginning of your script
+    const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
     text.enter()
       .append("text")
       .merge(text)
       .style("font-family", "Impact")
-      .style("fill", (d, i) => vis.fill(i))
+      .style("fill", d => d.color)
       .attr("text-anchor", "middle")
       .attr("font-size", d => d.size)
       .attr("transform", d => `translate(${d.x}, ${d.y})rotate(${d.rotate})`)
-      .text(d => d.text);
+      .text(d => d.text)
+      .on('mouseover', function(event, d) {
+        console.log(event);
+        tooltip.transition()
+          .duration(200)
+          .style("opacity", .9);
+        tooltip.html("Genre: " + d.genre)
+          .style("left", (event.pageX) + "px")
+          .style("top", (event.pageY - 28) + "px");
+        d3.select(this).style("fill", "white");
+      })
+      .on('mouseout', function() {
+        tooltip.transition()
+          .duration(500)
+          .style("opacity", 0);
+        d3.select(this).style("fill", b => b.color);
+      });
+
     text.exit().remove();
+  }
+
+  getPrimaryGenre(aritstName) {
+    let vis = this;
+    // Find the first occurrence of the artist in the data and return their genre
+    let artistData = vis.displayData.find(d => d.artist_names.includes(aritstName));
+    return artistData ? artistData.artist_genre : "Unknown";
+  }
+
+  getPrimaryArtist(data) {
+    return data.artist_names.split(",")[0];
   }
 }
