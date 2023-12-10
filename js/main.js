db3785ca65011604289f1c616e7a8b523af2317c
Soleil Saint-Cyr <saintcyrs@dhcp-10-250-151-123.harvard.edu>
2023-12-10 16:05:00 -0500

    Piano sidebar filled

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ function createVis(data) {
   barChart = new BarChartVis("barChart", data, "Oct26");
   barChart2 = new BarChartVis("barChart2", data, "Nov2");
   myWordCloud = new wordCloud("wordCloud", data, "Nov2");
-  myPiano = new Piano(data);
+  myPiano = new Piano(data, "Oct26");
   myClock = new clockVis("#songClock", data);
   myClock2 = new clockVis("#songClock2", data);
   myStickFigure = new stickFigure("#dancingStickFigure", {}, "Oct26");
--- js/piano.js
+++ js/piano.js
@@ -2,10 +2,11 @@
 
 class Piano {
   // Constructor
-  constructor(data) {
-
-    this.data = data;
-    this.playNote = this.playNote.bind(this);    this.hintsOn = this.hintsOn.bind(this);
+  constructor(data, date) {
+    this.date = date;
+    this.data = data.filter((d) => d.week === this.date);
+    this.playNote = this.playNote.bind(this);
+    this.hintsOn = this.hintsOn.bind(this);
 
     this.initVis();
   }
@@ -16,12 +17,44 @@ class Piano {
     vis.keys = document.querySelectorAll(".key");
     vis.note = document.querySelector(".nowplaying");
     vis.hints = document.querySelectorAll(".hints");
+    vis.sidebar = document.querySelector(".key-info");
 
     vis.wrangleData();
   }
   // Set up key counts 
   wrangleData() {
     let vis = this;
+      // TODO: Set up top 3 songs for each key
+      // TODO: Search through the data and find the top 3 songs for each key
+      // Object to hold top 3 songs for each key
+      vis.topSongs = {};
+
+      // Iterate through the data and populate topSongs
+    vis.data.forEach(song => {
+      let key = vis.fromPitchClass(song.key);
+
+      // Initialize the array for this key if it doesn't exist
+      if (!vis.topSongs[key]) {
+        vis.topSongs[key] = [];
+      }
+
+      // Create an object with the desired information
+      const songInfo = {
+        artistNames: song.artist_names,
+        trackName: song.track_name,
+        key: key
+      };
+
+      // Push the songInfo object to the array for its key
+      vis.topSongs[key].push(songInfo);
+
+      // Sort the array by streams (or another attribute) and keep only top 3
+      // Assuming 'streams' is a property in your data
+      vis.topSongs[key] = vis.topSongs[key].sort((a, b) => b.streams - a.streams).slice(0, 3);
+    });
+
+      console.log("Top Songs by Key", vis.topSongs);
+
       // Set up counts for each key
       const counts = {};
       vis.data.forEach((d) => {
@@ -40,7 +73,7 @@ class Piano {
         });
       });
 
-      console.log(vis.convertedCounts);
+      console.log("CONVERTED COUNTS", vis.convertedCounts);
       vis.updateVis();
     }
 
@@ -49,8 +82,12 @@ class Piano {
 
     vis.hints.forEach((hint, index) => vis.hintsOn(hint, index));
 
-    vis.keys.forEach(key => key.addEventListener("transitionend", vis.removeTransition));
-    window.addEventListener("keydown", vis.playNote);
+    // Add event listeners for both keyboard and mouse
+    vis.keys.forEach(key => {
+      key.addEventListener("transitionend", vis.removeTransition);
+      key.addEventListener("click", (e) => vis.playNote(e, key)); // Mouse click event
+    });
+    window.addEventListener("keydown", (e) => vis.playNote(e));
 
   }
 
@@ -72,26 +109,23 @@ fromPitchClass(pitchClass) {
   return pitchClasses[pitchClass];
 }
 
-playNote(e) {
+playNote(e, clickedKey = null) {
   let vis = this;
 
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
-    key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  // Determine whether the event is from a mouse click or a keyboard press
+  let keyCode = clickedKey ? clickedKey.getAttribute("data-key") : e.keyCode;
+  let key = document.querySelector(`.key[data-key="${keyCode}"]`);
+  let audio = document.querySelector(`audio[data-key="${keyCode}"]`);
 
-  if (!key) return;
+  if (!key || !audio) return;  // Exit if key or audio is not found
 
   const keyNote = key.getAttribute("data-note");
-
   key.classList.add("playing");
-  // Find the count for this key
-  const keyData = vis.convertedCounts.find(d => d.key === keyNote);
-  const keyCount = keyData ? keyData.count : 0;
-
-  // Update the display with key note and count
-  // TODO: Possibly change how this header works.
-  vis.note.innerHTML = `${keyNote} : ${keyCount} songs`;
   audio.currentTime = 0;
   audio.play();
+
+  // Update the sidebar with top songs for the key
+  vis.updateSidebar(keyNote);
 }
 
 removeTransition(e) {
@@ -103,4 +137,40 @@ hintsOn(e, index) {
   e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
 }
 
+updateSidebar(keyNote) {
+  let vis = this;
+
+  // Find the top songs for this key
+  let songsForKey = vis.topSongs[keyNote];
+
+  // Find the total number of songs for this key
+  let totalSongs = vis.convertedCounts.find(d => d.key === keyNote)?.count || 0;
+
+  // Clear existing content
+  vis.sidebar.innerHTML = '';
+
+  // Check if there are songs for this key
+  if (songsForKey && songsForKey.length > 0) {
+    // Create a header for the key information
+    let header = document.createElement('h3');
+    header.textContent = `Key of ${keyNote} - ${totalSongs} songs`;
+    vis.sidebar.appendChild(header);
+
+    // Create a list of songs
+    let list = document.createElement('ul');
+    songsForKey.forEach(song => {
+      let listItem = document.createElement('li');
+      listItem.textContent = `${song.artistNames} - ${song.trackName}`;
+      list.appendChild(listItem);
+    });
+
+    vis.sidebar.appendChild(list);
+  } else {
+    // Display a message if no songs are available for this key
+    vis.sidebar.textContent = `No top songs for the key of ${keyNote}.`;
+  }
 }
+
+
+}
+
