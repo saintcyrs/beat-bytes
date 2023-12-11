e7934954506eb972381f8af7c908915ae7eea837
Soleil Saint-Cyr <saintcyrs@dhcp-10-250-151-123.harvard.edu>
2023-12-11 09:24:37 -0500

    formatting

--- js/piano.js
+++ js/piano.js
@@ -11,6 +11,7 @@ class Piano {
     this.initVis();
   }
 
+  // Initialize visualization
   initVis() {
     let vis = this;
 
@@ -21,15 +22,16 @@ class Piano {
 
     vis.wrangleData();
   }
-  // Set up key counts 
+
+  // Set up key counts
   wrangleData() {
     let vis = this;
 
-      // Object to hold top 3 songs for each key
-      vis.topSongs = {};
+    // Object to hold top 5 songs for each key
+    vis.topSongs = {};
 
-      // Iterate through the data and populate topSongs
-    vis.data.forEach(song => {
+    // Iterate through the data and populate topSongs
+    vis.data.forEach((song) => {
       let key = vis.fromPitchClass(song.key);
 
       // Initialize the array for this key if it doesn't exist
@@ -41,135 +43,138 @@ class Piano {
       const songInfo = {
         artistNames: song.artist_names,
         trackName: song.track_name,
-        key: key
+        key: key,
       };
 
       // Push the songInfo object to the array for its key
       vis.topSongs[key].push(songInfo);
 
-      // Sort the array by streams (or another attribute) and keep only top 3
-      // Assuming 'streams' is a property in your data
-      vis.topSongs[key] = vis.topSongs[key].sort((a, b) => b.streams - a.streams).slice(0, 5);
+      // Sort the array by streams (or another attribute) and keep only top 5
+      vis.topSongs[key] = vis.topSongs[key]
+        .sort((a, b) => b.streams - a.streams)
+        .slice(0, 5);
     });
 
-      console.log("Top Songs by Key", vis.topSongs);
+    // Set up counts for each key
+    const counts = {};
+    vis.data.forEach((d) => {
+      let key = d.key;
+      if (counts[key]) {
+        counts[key] += 1;
+      } else {
+        counts[key] = 1;
+      }
+    });
 
-      // Set up counts for each key
-      const counts = {};
-      vis.data.forEach((d) => {
-        let key = d.key;
-        if (counts[key]) {
-          counts[key] += 1;
-        } else {
-          counts[key] = 1;
-        }
+    // Append key with counts to new array
+    vis.convertedCounts = [];
+    Object.keys(counts).forEach((numericKey) => {
+      vis.convertedCounts.push({
+        key: vis.fromPitchClass(numericKey),
+        count: counts[numericKey],
       });
-      vis.convertedCounts = [];
-      Object.keys(counts).forEach((numericKey) => {
-        vis.convertedCounts.push({
-          key: vis.fromPitchClass(numericKey),
-          count: counts[numericKey],
-        });
-      });
+    });
 
-      console.log("CONVERTED COUNTS", vis.convertedCounts);
-      vis.updateVis();
-    }
+    vis.updateVis();
+  }
 
   updateVis() {
     let vis = this;
 
+    // Creates hints on hover for each key
     vis.hints.forEach((hint, index) => vis.hintsOn(hint, index));
 
     // Add event listeners for both keyboard and mouse
-    vis.keys.forEach(key => {
+    vis.keys.forEach((key) => {
       key.addEventListener("transitionend", vis.removeTransition);
       key.addEventListener("click", (e) => vis.playNote(e, key)); // Mouse click event
     });
     window.addEventListener("keydown", (e) => vis.playNote(e));
-
   }
 
-fromPitchClass(pitchClass) {
-  const pitchClasses = {
-    0: "C",
-    1: "C#",
-    2: "D",
-    3: "D#",
-    4: "E",
-    5: "F",
-    6: "F#",
-    7: "G",
-    8: "G#",
-    9: "A",
-    10: "A#",
-    11: "B",
-  };
-  return pitchClasses[pitchClass];
-}
+  // Convert from pitch class to letter notation
+  fromPitchClass(pitchClass) {
+    const pitchClasses = {
+      0: "C",
+      1: "C#",
+      2: "D",
+      3: "D#",
+      4: "E",
+      5: "F",
+      6: "F#",
+      7: "G",
+      8: "G#",
+      9: "A",
+      10: "A#",
+      11: "B",
+    };
+    return pitchClasses[pitchClass];
+  }
 
-playNote(e, clickedKey = null) {
-  let vis = this;
+  // Play the note and update the sidebar
+  playNote(e, clickedKey = null) {
+    let vis = this;
 
-  // Determine whether the event is from a mouse click or a keyboard press
-  let keyCode = clickedKey ? clickedKey.getAttribute("data-key") : e.keyCode;
-  let key = document.querySelector(`.key[data-key="${keyCode}"]`);
-  let audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    // Determine whether the event is from a mouse click or a keyboard press
+    let keyCode = clickedKey ? clickedKey.getAttribute("data-key") : e.keyCode;
+    let key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    let audio = document.querySelector(`audio[data-key="${keyCode}"]`);
 
-  if (!key || !audio) return;  // Exit if key or audio is not found
+    if (!key || !audio) return; // Exit if key or audio is not found
 
-  const keyNote = key.getAttribute("data-note");
-  key.classList.add("playing");
-  audio.currentTime = 0;
-  audio.play();
+    const keyNote = key.getAttribute("data-note");
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+
+    // Update the sidebar with top songs for the key
+    vis.updateSidebar(keyNote);
+  }
+
+  // Remove the transition class
+  removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+  }
+
+  // Add hints on hover
+  hintsOn(e, index) {
+    e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+  }
 
   // Update the sidebar with top songs for the key
-  vis.updateSidebar(keyNote);
-}
+  updateSidebar(keyNote) {
+    let vis = this;
 
-removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  this.classList.remove("playing");
-}
+    // Find the top songs for this key
+    let songsForKey = vis.topSongs[keyNote];
 
-hintsOn(e, index) {
-  e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
-}
+    // Find the total number of songs for this key
+    let totalSongs =
+      vis.convertedCounts.find((d) => d.key === keyNote)?.count || 0;
 
-updateSidebar(keyNote) {
-  let vis = this;
+    // Clear existing content
+    vis.sidebar.innerHTML = "";
 
-  // Find the top songs for this key
-  let songsForKey = vis.topSongs[keyNote];
+    // Check if there are songs for this key
+    if (songsForKey && songsForKey.length > 0) {
+      // Create a header for the key information
+      let header = document.createElement("h3");
+      header.textContent = `Key of ${keyNote} - ${totalSongs} songs`;
+      vis.sidebar.appendChild(header);
 
-  // Find the total number of songs for this key
-  let totalSongs = vis.convertedCounts.find(d => d.key === keyNote)?.count || 0;
+      // Create a list of songs
+      let list = document.createElement("ul");
+      songsForKey.forEach((song) => {
+        let listItem = document.createElement("li");
+        listItem.textContent = `${song.artistNames} - ${song.trackName}`;
+        list.appendChild(listItem);
+      });
 
-  // Clear existing content
-  vis.sidebar.innerHTML = '';
-
-  // Check if there are songs for this key
-  if (songsForKey && songsForKey.length > 0) {
-    // Create a header for the key information
-    let header = document.createElement('h3');
-    header.textContent = `Key of ${keyNote} - ${totalSongs} songs`;
-    vis.sidebar.appendChild(header);
-
-    // Create a list of songs
-    let list = document.createElement('ul');
-    songsForKey.forEach(song => {
-      let listItem = document.createElement('li');
-      listItem.textContent = `${song.artistNames} - ${song.trackName}`;
-      list.appendChild(listItem);
-    });
-
-    vis.sidebar.appendChild(list);
-  } else {
-    // Display a message if no songs are available for this key
-    vis.sidebar.textContent = `No top songs for the key of ${keyNote}.`;
+      vis.sidebar.appendChild(list);
+    } else {
+      // Display a message if no songs are available for this key
+      vis.sidebar.textContent = `No top songs for the key of ${keyNote}.`;
+    }
   }
 }
-
-
-}
-
--- js/wordCloud/wordCloud.js
+++ js/wordCloud/wordCloud.js
@@ -12,12 +12,30 @@ class wordCloud {
     // Initialize minimum and maximum to be updated later
     this.minMax = [0, 100];
     this.displayData = this.data.filter((d) => d.week === this.date);
-    this.genreColorMap = d3.scaleOrdinal( [
-      "#645c9b", "#8e398d", "#698a8c", "#5e9178", "#915272", "#8a5274",
-      "#833cc6", "#5599be", "#4787b5", "#5860aa", "#9855a4", "#918072",
-      "#799965", "#8934a2", "#863e80", "#3869b8", "#789176", "#447dc4",
-      "#7a7a8f", "#5540a7"
-      ]);
+
+    // Custom color scale for genres
+    this.genreColorMap = d3.scaleOrdinal([
+      "#645c9b",
+      "#8e398d",
+      "#698a8c",
+      "#5e9178",
+      "#915272",
+      "#8a5274",
+      "#833cc6",
+      "#5599be",
+      "#4787b5",
+      "#5860aa",
+      "#9855a4",
+      "#918072",
+      "#799965",
+      "#8934a2",
+      "#863e80",
+      "#3869b8",
+      "#789176",
+      "#447dc4",
+      "#7a7a8f",
+      "#5540a7",
+    ]);
     this.initVis();
   }
 
@@ -43,18 +61,20 @@ class wordCloud {
         })`
       );
     vis.cloud = d3.layout.cloud().size([vis.width, vis.height]);
+    // Generate checkboxes
     vis.generateGenreOptions(vis.displayData);
+    // Initialize the word cloud
     vis.updateWordCloud();
   }
 
   wrangleData(selectedGenres) {
-    console.log(selectedGenres)
     let vis = this;
-    // Redefine displayData to contain the filtered list of just Nov2 data
 
-    vis.filteredData = vis.displayData.filter(d => {
+    vis.filteredData = vis.displayData.filter((d) => {
       // If no genres are selected, or the artist's genre is in the selected genres, include the data point
-      return d.artist_genre.split(',').some(genre => selectedGenres.includes(genre));
+      return d.artist_genre
+        .split(",")
+        .some((genre) => selectedGenres.includes(genre));
     });
 
     // Count the number of occurrences for each artist
@@ -64,8 +84,6 @@ class wordCloud {
       counts[artist] = (counts[artist] || 0) + 1;
     });
 
-    // Possibly redefine this to include the artist name ??
-    // BUT there are many artists who only have one song on the charts
     // Store the processed data for use in getWords
     // Modify the processedData mapping
     vis.processedData = Object.entries(counts).map(([artist, count]) => {
@@ -74,12 +92,12 @@ class wordCloud {
         artist_names: artist,
         genre: primaryGenre,
         count: count,
-        originalColor: vis.genreColorMap(primaryGenre) // Set the original color here
+        originalColor: vis.genreColorMap(primaryGenre), // Set the original color here
       };
     });
     // Calculate the min and max counts
     let min = Infinity,
-        max = -Infinity;
+      max = -Infinity;
     Object.values(counts).forEach((count) => {
       if (count < min) min = count;
       if (count > max) max = count;
@@ -88,30 +106,34 @@ class wordCloud {
     vis.wordScale = d3.scaleLinear().domain(vis.minMax).range([20, 80]);
     vis.showNewWords();
   }
-  
+
   // Encapsulate the word cloud functionality
   showNewWords() {
     let vis = this;
     // Calculate word sizes based on counts
-    let wordEntries = vis.getWords(vis.processedData).sort((a,b) => b.size - a.size);
-    
+    let wordEntries = vis
+      .getWords(vis.processedData)
+      .sort((a, b) => b.size - a.size);
+
     // Draw the word cloud with the new words
     vis.cloud
       .words(wordEntries)
       .padding(5)
-      .rotate(() => (~~(Math.random() * 2) * 90))
+      .rotate(() => ~~(Math.random() * 2) * 90)
       .font("Impact")
-      .fontSize(function(d) {
-        return d.size;})
-      .on("end", words => {
+      .fontSize(function (d) {
+        return d.size;
+      })
+      .on("end", (words) => {
         vis.draw(words);
       })
       .start();
   }
 
+  // Transform the data into a format suitable for the word cloud
   getWords(words) {
     let vis = this;
-    return words.map(d => ({
+    return words.map((d) => ({
       text: d.artist_names,
       genre: d.genre,
       size: vis.wordScale(d.count),
@@ -119,113 +141,121 @@ class wordCloud {
     }));
   }
 
+  // Draw the wordCloud
   draw(words) {
     let vis = this;
     // Bind data to text elements and update positions
-    let text = vis.svg.selectAll("text")
-      .data(words, d => d.text);
+    let text = vis.svg.selectAll("text").data(words, (d) => d.text);
 
-    // At the beginning of your script
-    const tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+    // Append the tooltip
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
 
-    text.enter()
+    // Enter new text elements
+    text
+      .enter()
       .append("text")
       .merge(text)
       .style("font-family", "Impact")
-      .style("fill", d => d.color)
+      .style("fill", (d) => d.color)
       .attr("text-anchor", "middle")
-      .attr("font-size", d => d.size)
-      .attr("transform", d => `translate(${d.x}, ${d.y})rotate(${d.rotate})`)
-      .text(d => d.text)
-      .on('mouseover', function(event, d) {
-        tooltip.transition()
-          .duration(200)
-          .style("opacity", .9);
-        tooltip.html("Genre: " + d.genre)
-          .style("left", (event.pageX) + "px")
-          .style("top", (event.pageY - 28) + "px");
+      .attr("font-size", (d) => d.size)
+      .attr("transform", (d) => `translate(${d.x}, ${d.y})rotate(${d.rotate})`)
+      .text((d) => d.text)
+      .on("mouseover", function (event, d) {
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip
+          .html("Genre: " + d.genre)
+          .style("left", event.pageX + "px")
+          .style("top", event.pageY - 28 + "px");
         d3.select(this).style("fill", "white");
       })
-      .on('mouseout', function() {
-        tooltip.transition()
-          .duration(500)
-          .style("opacity", 0);
-        d3.select(this).style("fill", b => b.color);
+      .on("mouseout", function () {
+        tooltip.transition().duration(500).style("opacity", 0);
+        d3.select(this).style("fill", (b) => b.color);
       });
 
     text.exit().remove();
   }
 
+  // Get primary genre for tooltip
   getPrimaryGenre(aritstName) {
     let vis = this;
     // Find the first occurrence of the artist in the data and return their genre
-    let artistData = vis.displayData.find(d => d.artist_names.includes(aritstName));
+    let artistData = vis.displayData.find((d) =>
+      d.artist_names.includes(aritstName)
+    );
     return artistData ? artistData.artist_genre : "Unknown";
   }
 
+  // Extract primary artist for wordcloud
   getPrimaryArtist(data) {
     return data.artist_names.split(",")[0];
   }
 
+  // Generate checkbox select menu
   generateGenreOptions(data) {
     let vis = this;
     // Create set of genres from data
-    vis.genres = new Set(data.map(d => d.artist_genre.split(",")).flat());
+    vis.genres = new Set(data.map((d) => d.artist_genre.split(",")).flat());
 
     // Select the container where checkboxes will be appended
-    const container = d3.select('.genre-select')
-        .style('overflow-y', 'scroll') // Add vertical scrollbar
-        .style('max-height', '500px'); // Set a maximum height
+    const container = d3
+      .select(".genre-select")
+      .style("overflow-y", "scroll") // Add vertical scrollbar
+      .style("max-height", "500px"); // Set a maximum height
 
     // Add 'All' checkbox first and set it to checked by default
-    let allLabel = container.append('label')
-    .attr('class', 'genre-label');
+    let allLabel = container.append("label").attr("class", "genre-label");
 
-    allLabel.append('input')
-      .attr('type', 'checkbox')
-      .attr('value', 'all')
-      .attr('checked', true) // Set checked attribute
-      .attr('class', 'genre-checkbox')
-      .on('change', function() {
-          // When 'All' is checked or unchecked, update all checkboxes
-          const isChecked = d3.select(this).property('checked');
-          d3.selectAll('.genre-checkbox').property('checked', isChecked);
-          vis.updateWordCloud();
+    allLabel
+      .append("input")
+      .attr("type", "checkbox")
+      .attr("value", "all")
+      .attr("checked", true) // Set checked attribute
+      .attr("class", "genre-checkbox")
+      .on("change", function () {
+        // When 'All' is checked or unchecked, update all checkboxes
+        const isChecked = d3.select(this).property("checked");
+        d3.selectAll(".genre-checkbox").property("checked", isChecked);
+        vis.updateWordCloud();
       });
 
-    allLabel.append('span').text(' all');
-    container.append('br'); // Line break after 'All'
-
+    allLabel.append("span").text(" all");
+    container.append("br"); // Line break after 'All'
 
     // Add checkboxes and labels for other genres
-    vis.genres.forEach(genre => {
-        if (genre !== "All") {
-            let label = container.append('label')
-                .attr('class', 'genre-label');
+    vis.genres.forEach((genre) => {
+      if (genre !== "All") {
+        let label = container.append("label").attr("class", "genre-label");
 
-            label.append('input')
-                .attr('type', 'checkbox')
-                .attr('value', genre)
-                .attr('checked', true)
-                .attr('class', 'genre-checkbox')
-                .on('change', function() {
-                    vis.updateWordCloud();
-                });
+        label
+          .append("input")
+          .attr("type", "checkbox")
+          .attr("value", genre)
+          .attr("checked", true)
+          .attr("class", "genre-checkbox")
+          .on("change", function () {
+            vis.updateWordCloud();
+          });
 
-            label.append('span')
-                .text(` ${genre}`);
+        label.append("span").text(` ${genre}`);
 
-            container.append('br'); // Line break after each genre
-        }
+        container.append("br"); // Line break after each genre
+      }
     });
-}
+  }
 
+  // Update the word cloud when the checkboxes are changed
   updateWordCloud() {
     let vis = this;
-    const selectedGenres = d3.selectAll('.genre-checkbox:checked').nodes()
-      .map(node => node.value);
+    const selectedGenres = d3
+      .selectAll(".genre-checkbox:checked")
+      .nodes()
+      .map((node) => node.value);
     vis.wrangleData(selectedGenres);
   }
 }
